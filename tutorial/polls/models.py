6d2e4b3bbbcaf3c640a4c5e78a1aeaf1eb774f00
Stefano DAmico <skdamico@gmail.com>
2012-03-29 16:52:09 -0400

    initial commit of tutorial

---
+++ tutorial/polls/models.py
@@ -0,0 +1,21 @@
+import pytz
+import datetime
+from django.db import models
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= datetime.datetime.now(pytz.utc) - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
