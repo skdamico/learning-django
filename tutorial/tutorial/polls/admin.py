37dfca6baebf879042bb4f3260c7ec83c9a62bcd
Stefano DAmico <skdamico@gmail.com>
2012-04-04 14:23:08 -0400

    Finished up to part 3

---
+++ tutorial/tutorial/polls/admin.py
@@ -0,0 +1,6 @@
+from tutorial.polls.models import Poll
+from tutorial.polls.models import Choice
+from django.contrib import admin
+
+admin.site.register(Poll)
+admin.site.register(Choice)
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -109,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/stef/dev/learning-django/tutorial/tutorial/templates'
 )
 
 INSTALLED_APPS = (
@@ -119,10 +120,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'polls',
+    'tutorial.polls',
 )
 
 # A sample logging configuration. The only tangible logging
